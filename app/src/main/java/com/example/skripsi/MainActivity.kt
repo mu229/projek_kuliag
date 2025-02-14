68baabfebaee8039558d6147c6742a23e24feedf
mu229 <121448146+mu229@users.noreply.github.com>
2025-02-14 09:20:16 +0700

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/skripsi/MainActivity.kt
+++ app/src/main/java/com/example/skripsi/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.skripsi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val viewPager = findViewById<ViewPager2>(R.id.view_pager)
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+
+        viewPager.adapter = ViewPagerAdapter(this)
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when(position) {
+                0 -> "Peta Kesehatan"
+                1 -> "Fragment 2"
+                else -> "Fragment 3"
+            }
+        }.attach()
     }
 }
